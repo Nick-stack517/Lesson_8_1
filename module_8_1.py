6e736e6feec54226db5fb4304ca6a289c11037e9
Nick-stack517 <gsatello@gmail.com>
2024-11-15 08:55:53 +0700

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,22 @@
+﻿"""
+2023/11/24 00:00|Домашнее задание по теме "Try и Except"
+
+Задание "Программистам всё можно":
+Пора разрушать шаблоны привычного нам Python! Вот вас раздражает, что мы не
+можем сложить число(int) и строку(str)? Давайте исправим это недоразумение!
+"""
+
+
+def add_everything_up(a, b):
+    try:
+        res = a + b
+        res = round(res, 3)
+    except TypeError:
+        # print("Чейто не того")
+        res = str(a) + str(b)
+    return res
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
